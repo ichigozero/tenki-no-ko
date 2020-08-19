32d1bdebe19b86ee90187d3fe1274aaba2730136
Gary Sentosa <gary.sentosa@gmail.com>
2020-08-20 07:55:53 +0900

    Add missing tests

--- tests/test_location_scraper.py
+++ tests/test_location_scraper.py
@@ -14,6 +14,18 @@ def test_extract_regions(mocker, index_html, location_scraper):
     assert output['10'] == '沖縄地方'
 
 
+def test_failed_to_extract_regions(mocker, location_scraper):
+    mock_get_soup = mocker.patch.object(
+        target=location_scraper,
+        attribute='get_soup',
+        return_value=None
+    )
+    output = location_scraper.extract_regions()
+
+    mock_get_soup.assert_called_once_with('https://tenki.jp')
+    assert output is None
+
+
 def test_extract_prefectures(mocker, prefecture_html, location_scraper):
     mock_get_soup = mocker.patch.object(
         target=location_scraper,
@@ -25,6 +37,18 @@ def test_extract_prefectures(mocker, prefecture_html, location_scraper):
     mock_get_soup.assert_called_once_with('https://tenki.jp/forecast/3/')
     assert output['16'] == '東京都'
     assert output['23'] == '長野県'
+
+
+def test_failed_to_extract_prefectures(mocker, location_scraper):
+    mock_get_soup = mocker.patch.object(
+        target=location_scraper,
+        attribute='get_soup',
+        return_value=None
+    )
+    output = location_scraper.extract_prefectures(region_id=3)
+
+    mock_get_soup.assert_called_once_with('https://tenki.jp/forecast/3/')
+    assert output is None
 
 
 def test_extract_subprefectures_and_cities(
@@ -54,3 +78,18 @@ def test_extract_subprefectures_and_cities(
     mock_get_soup.assert_called_once_with('https://tenki.jp/forecast/3/16/')
     assert output['東京23区']['13101'] == expected_value_1
     assert output['小笠原諸島(父島)']['13421'] == expected_value_2
+
+
+def test_failed_to_extract_prefectures_and_cities(mocker, location_scraper):
+    mock_get_soup = mocker.patch.object(
+        target=location_scraper,
+        attribute='get_soup',
+        return_value=None
+    )
+    output = location_scraper.extract_subprefectures_and_cities(
+        region_id=3,
+        prefecture_id=16
+    )
+
+    mock_get_soup.assert_called_once_with('https://tenki.jp/forecast/3/16/')
+    assert output is None
